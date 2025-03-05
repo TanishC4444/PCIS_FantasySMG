c01f8cb05e8b4536479319833d42b618c523aa8a
Tanish Chauhan <tanishchauhan4444@gmail.com>
2025-03-04 18:44:28 -0600

    EXEv3

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 import webbrowser
-import os
+import threading
 
-# Command Line: auto-py-to-exe
-# Path: /Users/tanishchauhan/Desktop/PCIS_FantasySMG/app.py
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    # Automatically open the URL in Google Chrome on macOS
+def open_browser():
     webbrowser.get('open -a "Google Chrome" %s').open("http://127.0.0.1:5000/")
+
+if __name__ == '__main__':
+    # Start a timer to open the browser after Flask starts
+    threading.Timer(1.5, open_browser).start()  # Delay by 1.5 seconds
     app.run(debug=True)
